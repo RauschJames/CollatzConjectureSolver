ff87505c66d46d2942a1fc45b1be3bef9bc6d541
RauschJames <76003622+RauschJames@users.noreply.github.com>
2022-07-23 11:20:41 -0700

    Update NumberGenerator.py

--- NumberGenerator.py
+++ NumberGenerator.py
@@ -1,32 +1,32 @@
 class NumberGenerator():
 	def __init__():
-		//nothing  to do
-	def generatePath(int x): //returns an array with the path of X to 1
+		#nothing  to do
+	def generatePath(int x): #returns an array with the path of X to 1
 		path = []
-		path.append(x) //starter makes sure 1 is included
+		path.append(x) #starter makes sure 1 is included
 		while(x != 1):
-			if(x % 2 != 0): //odd
+			if(x % 2 != 0): #odd
 				x = 3*x + 1
 				path.append(x)
 			 else 
 				x = x / 2;
 				path.append(x)
 		return path
-	def stepsToOne(int x): //return counter of steps to 1
+	def stepsToOne(int x): #return counter of steps to 1
 		steps = 0
 		while(x != 1):
-			if(x % 2 != 0): //odd
+			if(x % 2 != 0): #odd
 				steps += stepsToOne(3 * x + 1)
 			else:
 				steps += stepsToOne(x / 2)
 		return steps
-	def printPath(int x): //like generatePath but just prints them
+	def printPath(int x): #like generatePath but just prints them
 		while(x != 1):
-			if(x % 2 != 0): //odd
+			if(x % 2 != 0): #odd
 				x = 3*x + 1
-				print(" x ")
+				print(" " + x +" ")
 			 else 
 				x = x / 2;
-				print(" x ")
+				print(" " + x +" ")
 
 	
